7129f2592f6626bd1b75f1b45bb578976cd9fc4f
Skyvot <xuhongzhi@SkyvotdeMBP.lan>
2022-11-19 10:41:29 +0800

    feat: update problem 814

---
+++ tree/814-binary-tree-pruning.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+ function pruneTree(root: TreeNode | null): TreeNode | null {
+    if(!root) return null;
+    const leftNode = pruneTree(root.left);
+    const rightNode = pruneTree(root.right);
+    const newNode = new TreeNode(root.val, leftNode, rightNode);
+    if(!leftNode && !rightNode && !root.val) return null;
+    return newNode;
+};
